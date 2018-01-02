cd29503fd2b0fe4fc60775b5d1661ec81019dfeb
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-01 21:58:12 -0500

    ex1ba78db3

--- ex1bankacc_db.rb
+++ ex1bankacc_db.rb
@@ -1,6 +1,13 @@
 #object Oriented Programming: Class Methods and Class Variables
-
 require './ex1bankacc.rb'
+
+
 
 my_account = BankAccount.create
 your_account = BankAccount.create
+
+p my_account.balance(200)
+p your_account.balance(1000)
+
+p BankAccount.total_funds
+p BankAccount.interest_time
