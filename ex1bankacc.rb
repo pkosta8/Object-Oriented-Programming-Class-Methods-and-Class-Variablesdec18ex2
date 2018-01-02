e0709105bcddf5d66bf96fe7edc6ff92d7281aa8
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-01 21:54:40 -0500

    ex1ba78db

--- ex1bankacc.rb
+++ ex1bankacc.rb
@@ -21,3 +21,27 @@ end
 def withdrawl(amount)
 @balance = @balance - amount
 end
+
+
+def balance
+return @balance
+end
+
+def balance=(new_balance)
+@balance = new_balance
+end
+
+def self.create
+my_account = BankAccount.new
+@@accounts << my_account
+return my_account
+end
+
+def self.total_funds
+total = 0
+@@accounts.each do |bank_account|
+total = total + bank_account.balance
+end
+
+return total
+end
